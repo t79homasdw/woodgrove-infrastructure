1bb3652d57527edf216aff32e00bfcfbdcf20da3
t79homasdw <dan.thomas@3cloudsolutions.com>
2025-09-06 23:48:39 -0400

    push updates

--- import_secrets.py
+++ import_secrets.py
@@ -3,9 +3,9 @@ import subprocess
 # List of secrets to import
 secrets_to_import = [
     {
-        "vault_name": "kv-3cloud-dev-demo",
+        "vault_name": "kv-<DEMO-NAME>-dev-demo",
         "secret_name": "backup-sas-token",
-        "version": "70420b1f73864833afbe49415598fdb5"
+        "version": "11111111111111111111111"
     },
     # Add more secrets here
     # {"vault_name": "your-vault", "secret_name": "your-secret", "version": "optional-version"},
